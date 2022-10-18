2620a2411ec654ddf8ccfb52722d84799d2ced3d
Gorkasmatzaile <gorkainventor@gmail.com>
2022-10-18 18:50:48 +0200

    [mastermind] complete MVP

--- ruby-course/mastermind/mastermind.rb
+++ ruby-course/mastermind/mastermind.rb
@@ -20,9 +20,62 @@ end
 class Game
   include Helpers
 
+  private
+
   def initialize
-    # @code = Array.new(4) {rand(6)}
-    @code = [3, 2, 2, 1]
+    @code = Array.new(4) { rand(6) }
+    @number_of_guesses = 12
+    play
+  end
+
+  def play
+    puts inital_text
+    number_of_guesses.times do |i|
+      @guess_number = i + 1
+      guess = ask_guess
+      p feedback = check(guess)
+      return puts 'You won!' if feedback.all?('fully_right')
+    end
+    puts 'You lost :('
+  end
+
+  def inital_text
+    "======================
+Welcome to MasterMind!
+======================
+Your goal is to crack the computer's code, which consists in four digits ranging from 0 to 5.
+Each round, you will enter a code, and you will receive feedback on how well each digit resembles the computer's code.
+  - 'fully_right': you matched the number and the position!
+  - 'semi_right': you matched the number, but it is not in the correct position.
+  - 'wrong': you didn't match the number. Better luck next time!
+======================"
+  end
+
+  def ask_guess
+    puts "Guess #{@guess_number}. #{number_of_guesses - @guess_number} guesses more remaining."
+    puts 'Please enter your guess as a comma-separated list (e.g. 1, 2, 3, 4):'
+    @input_array = gets.chomp.split(', ')
+    return parse(@input_array) unless @input_array.length > 4
+
+    wrong_input
+  end
+
+  def parse(array)
+    array = array.map do |element|
+      element_i = element.to_i
+      break wrong_input unless element.to_i.to_s == element && element_i > -1 && element_i < 7
+
+      element_i
+    end
+    (4 - array.length).times do
+      array.push(-1)
+    end
+    array
+  end
+
+  def wrong_input
+    puts "Wrong input: #{@input_array}"
+    ask_guess
   end
 
   def check(guess)
@@ -33,21 +86,16 @@ class Game
       code_available[element] -= 1 if is_fully_right[index]
     end
 
-    feedback =
-      guess.map.with_index do |element, index|
-        next 'fully_right' if is_fully_right[index]
-        next false unless code_available[element].positive?
+    guess.map.with_index do |element, index|
+      next 'fully_right' if is_fully_right[index]
+      next 'wrong' unless code_available[element].positive?
 
-        code_available[element] -= 1
-        'semi_right'
-      end
-    p feedback
+      code_available[element] -= 1
+      'semi_right'
+    end
   end
 
-  private
-
-  attr_reader :code
+  attr_reader :code, :number_of_guesses
 end
 
-game = Game.new
-game.check([1, 2, 1, 9])
+Game.new
