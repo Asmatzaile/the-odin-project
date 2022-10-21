9ebbb36e6a4e512a0275973d83d28e017ab987d7
Gorkasmatzaile <gorkainventor@gmail.com>
2022-10-21 18:19:32 +0200

    [mastermind] add code-maker gamemode

--- ruby-course/mastermind/README.md
+++ ruby-course/mastermind/README.md
@@ -1,2 +1,7 @@
-A Mastermind game from the command line where you have 12 turns to guess the secret code, starting with you guessing the computer’s random code.
-Part of the The Odin Project curriculum.
+## Mastermind
+Part of the The Odin Project's curriculum.
+## Game description
+A game where one of the players has 12 turns to guess the secret code set by its opponent. After each guess, they receive feedback on the digits that were right, or if that digit was also in the correct place.
+## Instructions
+This Mastermind game is played in the command line against the computer. You can choose to be the guesser, and try to break the computer's randomly generated code, or to be the code maker, and create the code for the computer to break. Additional instructions will be displayed during the game in the console.
+To play this game, download the repository and run mastermind.rb in the command line.
--- ruby-course/mastermind/mastermind.rb
+++ ruby-course/mastermind/mastermind.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Helper functions for Game class
+# Helper functions
 module Helpers
   def count_elements(array)
     array.reduce(Hash.new(0)) do |hash, element|
@@ -16,6 +16,29 @@ module Helpers
   end
 end
 
+# Text storage
+module Text
+  TITLE =
+'======================
+Welcome to MasterMind!
+======================'
+
+  ASK_GAMEMODE =
+'Do you want to:
+  1. guess the code
+  2. create the code?'
+
+  HUMANGOAL_CRACK =
+"Your goal is to crack the computer's code, which consists in four digits ranging from 0 to 5.
+Each round, you will enter a code, and you will receive feedback on how well each digit resembles the computer's code.
+  - 'fully_right': you matched the number and the position!
+  - 'semi_right': you matched the number, but it is not in the correct position.
+  - 'wrong': you didn't match the number. Better luck next time!"
+
+  HUMANGOAL_CODE =
+"Your goal is to write a code that the computer won't crack. The code must consist in four digits ranging from 0 to 5."
+end
+
 # Main class
 class Game
   include Helpers
@@ -23,59 +46,40 @@ class Game
   private
 
   def initialize
-    @code = Array.new(4) { rand(6) }
+    computer = Computer.new
+    human = Human.new
+    puts Text::TITLE
+    puts Text::ASK_GAMEMODE
+    @gamemode = human.ask_gamemode
+    puts "You chose gamemode #{@gamemode}."
+    set_roles(computer, human)
     @number_of_guesses = 12
     play
   end
 
+  def set_roles(computer, human)
+    if @gamemode == 1
+      @coder = computer
+      @guesser = human
+      puts Text::HUMANGOAL_CRACK
+    else
+      @coder = human
+      @guesser = computer
+      puts Text::HUMANGOAL_CODE
+    end
+  end
+
   def play
-    puts inital_text
+    @code = coder.ask_code
     number_of_guesses.times do |i|
-      @guess_number = i + 1
-      guess = ask_guess
-      p feedback = check(guess)
-      return puts 'You won!' if feedback.all?('fully_right')
+      guess_number = i + 1
+      puts "Guess #{guess_number}. #{@number_of_guesses - guess_number} guesses more remaining."
+      guess = guesser.ask_guess
+      feedback = check(guess)
+      guesser.feedback(feedback)
+      return guesser.win if feedback.all?('fully_right')
     end
-    puts 'You lost :('
-  end
-
-  def inital_text
-    "======================
-Welcome to MasterMind!
-======================
-Your goal is to crack the computer's code, which consists in four digits ranging from 0 to 5.
-Each round, you will enter a code, and you will receive feedback on how well each digit resembles the computer's code.
-  - 'fully_right': you matched the number and the position!
-  - 'semi_right': you matched the number, but it is not in the correct position.
-  - 'wrong': you didn't match the number. Better luck next time!
-======================"
-  end
-
-  def ask_guess
-    puts "Guess #{@guess_number}. #{number_of_guesses - @guess_number} guesses more remaining."
-    puts 'Please enter your guess as a comma-separated list (e.g. 1, 2, 3, 4):'
-    @input_array = gets.chomp.split(', ')
-    return parse(@input_array) unless @input_array.length > 4
-
-    wrong_input
-  end
-
-  def parse(array)
-    array = array.map do |element|
-      element_i = element.to_i
-      break wrong_input unless element.to_i.to_s == element && element_i > -1 && element_i < 7
-
-      element_i
-    end
-    (4 - array.length).times do
-      array.push(-1)
-    end
-    array
-  end
-
-  def wrong_input
-    puts "Wrong input: #{@input_array}"
-    ask_guess
+    guesser.lose
   end
 
   def check(guess)
@@ -95,7 +99,99 @@ Each round, you will enter a code, and you will receive feedback on how well eac
     end
   end
 
-  attr_reader :code, :number_of_guesses
+  attr_reader :number_of_guesses, :code, :coder, :guesser
+end
+
+# Methods common to both human and computer
+class Player
+  def feedback(feedback)
+    p feedback
+  end
+end
+
+# Human methods
+class Human < Player
+  def wrong_input(callback)
+    puts 'Wrong input.'
+    callback.call
+  end
+
+  def ask_gamemode
+    puts 'Please enter 1 or 2.'
+    input = gets.chomp
+    input_i = input.to_i
+    if input_i.to_s == input && [1, 2].include?(input_i)
+      input_i
+    else
+      wrong_input(method(:ask_gamemode))
+    end
+  end
+
+  def ask_code
+    puts 'Please enter the code as a comma-separated list (e.g. 0, 2, 3, 4):'
+    input_array = gets.chomp.split(', ')
+    if input_array.length <= 4 && input_array.all? { |x| x.to_i.to_s == x && x.to_i > -1 && x.to_i < 6 }
+      input_array.map(&:to_i)
+    else
+      wrong_input(method(:ask_code))
+    end
+  end
+
+  def ask_guess
+    puts 'Please enter your guess as a comma-separated list (e.g. 0, 2, 3, 4):'
+    input_array = gets.chomp.split(', ')
+    if input_array.length > 4 && input_array.all? { |x| x.to_i.to_s == x && x.to_i  > -1 && x.to_i  < 6 }
+      array = input_array.map(&:to_i)
+      (4 - array.length).times { array.push(-1) }
+      array
+    else
+      wrong_input(method(:ask_guess))
+    end
+  end
+
+  def win
+    puts 'You won!'
+  end
+
+  def lose
+    puts 'You lost :('
+  end
+end
+
+# Computer methods
+class Computer < Player
+  def initialize
+    super
+    @memo = Array.new(4)
+  end
+
+  def ask_code
+    Array.new(4) { rand(6) }
+  end
+
+  # algorithm to crack the code
+  def ask_guess
+    @guess = @memo
+    @guess = @guess.map { |x| x.nil? ? rand(6) : x }
+    puts "Computer guessed #{@guess}"
+    @guess
+  end
+
+  def feedback(feedback_arr)
+    super
+    right_pos_arr = feedback_arr.each_index.select { |i| feedback_arr[i] == 'fully_right' }
+    right_pos_arr.each do |i|
+      @memo[i] = @guess[i]
+    end
+  end
+
+  def win
+    puts 'Computer won'
+  end
+
+  def lose
+    puts 'Computer lost :)'
+  end
 end
 
 Game.new
