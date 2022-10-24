8625cd72940cb72fa0d3b652f34e8ab5fe532364
Gorkasmatzaile <gorkainventor@gmail.com>
2022-10-24 04:22:08 +0200

    [hangman] add save functionality

--- ruby-course/hangman/hangman.rb
+++ ruby-course/hangman/hangman.rb
@@ -1,20 +1,89 @@
 # frozen_string_literal: true
 
+require 'yaml'
 # Clase
 class Game
   def initialize
-    file = File.open('google-10000-english-no-swears.txt')
-    @dictionary = []
-    @dictionary.push(file.readline.strip) until file.eof? # readlines messed up with gets and I had to use $stdin.gets every time I wanted to use it.
-    newgame
+    puts "===========
+  HANGMAN
+==========="
+    if load_or_create == 'load'
+      load_game
+    else
+      create_game
+      play
+    end
   end
 
-  def newgame
-    @word = select_word(@dictionary)
+  def load_or_create
+    return 'create' unless check_saves
+
+    loop do
+      puts "Do you want to  1) Load a previous game
+                2) Play a new game"
+      input = gets.chomp
+      return 'load' if input == '1'
+      return 'create' if input == '2'
+
+      'Wrong input'
+    end
+  end
+
+  def check_saves
+    Dir.exist?('saves') && !Dir.empty?('saves')
+  end
+
+  def create_game
+    file = File.open('google-10000-english-no-swears.txt')
+    dictionary = []
+    dictionary.push(file.readline.strip) until file.eof? # readlines messed up with gets and I had to use $stdin.gets every time I wanted to use it.
+    @word = select_word(dictionary)
     @wrong_letters = []
     @correct_letters = []
     @lives = 12
-    play
+  end
+
+  def save_game
+    name = ''
+    loop do
+      puts 'Enter a name for your save:'
+      name = gets.chomp
+      break unless File.exist?("saves/#{name}")
+      break if prompt_overwrite(name) == 'y'
+    end
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+    serialized_class = YAML.dump(self)
+    File.write("saves/#{name}", serialized_class)
+    puts 'Game saved!'
+  end
+
+  def prompt_overwrite(name)
+    loop do
+      puts "File \"#{name}\" already exists. Overwrite? (Y/n)"
+      input = gets.chomp.downcase
+      return input if %w[y n].include?(input)
+
+      puts 'Wrong input'
+    end
+  end
+
+  def load_game
+    game_array = Dir.entries('saves')
+    game_array.pop(2)
+    puts 'Your saved games are:'
+    game_array.each_with_index do |name, index|
+      puts "#{index + 1}) \"#{name}\""
+    end
+    input = ''
+    loop do
+      puts 'Enter a number to load your game'
+      input = gets.chomp
+      break if input.to_i.to_s == input && input.to_i.between?(1, game_array.length)
+
+      puts 'Wrong input'
+    end
+    save = File.open("saves/#{game_array[input.to_i - 1]}")
+    YAML.load(save, permitted_classes: ['Game']).play
   end
 
   def select_word(dictionary, min_length = 5, max_length = 12)
@@ -35,10 +104,12 @@ class Game
 
       # maybe win condition should be determined in another way;
       # I don't think display should be a class variable.
-
-      guess = ask_input
-
-      check_guess(guess)
+      if guess_or_save == 'guess'
+        guess = ask_guess
+        check_guess(guess)
+      else
+        save_game
+      end
     end
     newgame if prompt_replay
   end
@@ -48,6 +119,20 @@ class Game
     puts "Lives left: #{@lives}"
     puts "Wrong letters: #{@wrong_letters.join(', ')}"
     puts "Word: #{@display}"
+  end
+
+  def guess_or_save
+    return 'guess' if (@wrong_letters + @correct_letters).empty?
+
+    loop do
+      puts 'Do you want to  1) Guess a letter
+                2) Save the current game'
+      input = gets.chomp
+      return 'guess' if input == '1'
+      return 'save' if input == '2'
+
+      puts 'Wrong input'
+    end
   end
 
   def check_guess(guess)
@@ -78,22 +163,22 @@ class Game
     end
   end
 
-  def ask_input
-    puts 'Enter your guess'
-    input = $stdin.gets.chomp.to_s.downcase
+  def ask_guess
+    puts 'Enter a letter'
+    guess = gets.chomp.to_s.downcase
 
-    validate_input(input)
+    validate_guess(guess)
   end
 
-  def validate_input(input)
-    if input.match?(/^[a-z]$/)
-      return input unless @wrong_letters.include?(input) || @correct_letters.include?(input)
+  def validate_guess(guess)
+    if guess.match?(/^[a-z]$/)
+      return guess unless (@wrong_letters + @correct_letters).include?(guess)
 
-      puts "You already guessed #{input}."
+      puts "You already guessed #{guess}."
     else
       puts 'Wrong input'
     end
-    ask_input
+    ask_guess
   end
 end
 
